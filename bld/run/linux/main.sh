bb295e4c3414bb8697e966dc56ddc1046fb658cf
q4niel <github.obscure938@passmail.net>
2025-06-30 18:27:07 +0200

    linux venv setup, with basic python entry point

---
+++ bld/run/linux/main.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd "$(dirname $(dirname $(dirname $(dirname $(realpath "$0")))))"
+source "bld/run/linux/util.sh"
+runPy bld/src/main.py
---
+++ bld/run/linux/util.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+runPy() {
+    if [[ "$(uname)" != "Linux" ]]; then
+        echo -e "This script must run on Linux!"
+        exit 1
+    fi
+
+    if ! [ -d "bld/venv" ]; then
+        python3 -m venv bld/venv
+        source bld/venv/bin/activate
+        pip install lupa
+    else
+        source bld/venv/bin/activate
+    fi
+
+    python3 $1
+    deactivate
+}
