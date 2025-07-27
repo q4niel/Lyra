2d07bdeee15a6242e5e1882d4b0e3ee108d7fad7
q4niel <github.obscure938@passmail.net>
2025-07-27 16:52:01 +0200

    build directory permissions

--- bld/run/linux/build.sh
+++ bld/run/linux/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 cd "$(dirname $(dirname $(dirname $(dirname $(realpath "$0")))))"
 source "bld/run/linux/util.sh"
+time=$(date +"%Y_%m_%d-%H_%M_%S")
+
 runDocker bld/src/build.py
 if [ ! -d bld/out ]; then
     mkdir bld/out
 fi
 copyDocker "bld/out" "bld/out"
-sudo mv "bld/out/out" "bld/out/$(date +"%Y_%m_%d-%H_%M_%S")"
+sudo mv "bld/out/out" "bld/out/$time"
+sudo chown -R $(whoami):$(whoami) "bld/out/$time"
