9fe4b96c6da3fc06e77c2236d23ebb6108a8c346
q4niel <github.obscure938@passmail.net>
2025-07-31 16:50:57 +0200

    fix bug where object files were incorrectly named

--- bld/src/util/clang.py
+++ bld/src/util/clang.py
@@ -45,7 +45,7 @@ def build (
                 print(f"License '{lic}' for '{bin}' is undefined.")
 
     def compileSource(src: str) -> None:
-        obj: str = f"{os.path.basename(src)[:-4]}.o"
+        obj: str = f"{os.path.basename(src).rsplit(".", 1)[0]}.o"
         print(f"Compiling {src}...")
         os.system(f"clang++ {_join(compileFlags)} {_join(libIncludes)} -c src/{src} -o {objectDir}/{obj}")
         objects.append(f"{objectDir}/{obj}")
