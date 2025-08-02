de74a13c7fe0d4f3cda6c2f6a754197e2322bfbf
q4niel <github.obscure938@passmail.net>
2025-08-02 14:08:19 +0200

    Disable Raylib window exit key in release build.

--- src/engine/engine.cxx
+++ src/engine/engine.cxx
@@ -5,10 +5,15 @@ bool eng::init() {
     #ifndef DEBUG
         SetTraceLogLevel(LOG_NONE);
     #endif
+
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(800, 600, "My Window");
 
     if (!IsWindowReady()) return false;
+
+    #ifndef DEBUG
+        SetExitKey(KEY_NULL);
+    #endif
 
     SetTargetFPS(60);
     return true;
